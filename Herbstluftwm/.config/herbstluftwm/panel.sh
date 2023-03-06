62bcc43068c55aaa6ee3ad0c7371d23af17df517
Norel <norel.evoagile@gmail.com>
2023-03-06 22:36:04 +0900

    Fix polybar panel setting

--- Herbstluftwm/.config/herbstluftwm/panel.sh
+++ Herbstluftwm/.config/herbstluftwm/panel.sh
@@ -8,10 +8,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload -c $HOME/.config/herbstluftwm/polybar_void_config mainbar-herbst &
+    MONITOR=$m polybar --reload -c $HOME/.config/polybar/config.ini mainbar-herbst &
   done
 else
-  polybar --reload -c $HOME/.config/herbstluftwm/polybar_void_config mainbar-herbst &
+  polybar --reload -c $HOME/.config/polybar/config.ini mainbar-herbst &
 fi
 
 echo "Bars launched..."
