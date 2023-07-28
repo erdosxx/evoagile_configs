5c9a1da91cf1861ccf0e7b01cb14a096aa4629b3
Norel <norel.evoagile@gmail.com>
2023-07-28 20:43:38 +0900

    Update startup.jl with supporting username.

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,78 +1,61 @@
-# Display the entire type hierarchy starting from
-# the specified `roottype`
-function subtypetree(roottype, level = 1, indent = 4)
-  level == 1 && println(roottype)
-  for s in subtypes(roottype)
-    println(join(fill(" ", level * indent)) * string(s))
-    subtypetree(s, level + 1, indent)
-  end
-end
+using PkgTemplates
+using LibGit2
 
 """
-    template()
+    genGithubRepo(userName, repoName)
+
 Generate GitHub repository with GitHubActions.
 
 To generate package,
 in the julia REPL
 ```julia
-julia> generate(templateGithub, "<repoName>.jl")
+julia> genGithubRepo("evoagile", "repoName.jl")
 ```
-
 """
-function genGithubRepo()
-  @eval begin
-    using PkgTemplates
-    using LibGit2
-    templateGithub = Template(;
-      user = "evoagile",   # evoagile for organization
-      dir = "~/localgit",
-      julia = v"1.8",  # minimum allowed julia version
-      plugins = [
-        ProjectFile(; version = v"0.0.1-DEV"),
-        License(; name = "MIT", path = nothing, destination = "LICENSE"),
-        Git(;
-          branch = LibGit2.getconfig("init.defaultBranch", "master"),
-          ssh = true,
-          jl = true,
-          manifest = false,
-        ),
-        GitHubActions(;
-          destination = "CI.yml",
-          linux = true,
-          osx = false,
-          windows = false,
-          x64 = true,
-          x86 = false,
-          coverage = true,
-          extra_versions = ["1.8", "1.9", "nightly"],
-        ),
-        CompatHelper(; destination = "CompatHelper.yml", cron = "0 0 * * *"),
-        TagBot(;
-          destination = "TagBot.yml",
-          trigger = "JuliaTagBot",
-          token = Secret("GITHUB_TOKEN"),
-          ssh = Secret("DOCUMENTER_KEY"),
-          ssh_password = nothing,
-          changelog = nothing,
-          changelog_ignore = nothing,
-          gpg = nothing,
-          gpg_password = nothing,
-          registry = nothing,
-          branches = nothing,
-          dispatch = nothing,
-          dispatch_delay = nothing,
-        ),
-        Codecov(),
-        Documenter{GitHubActions}(
-          logo = Logo(;
-            light = homedir() * "/.config/logo/logo.png",
-            dark = homedir() * "/.config/logo/logo-dark.png",
-          ),
-        ),
-        Dependabot(),
-      ],
-    )
-  end
+function genGithubRepo(userName::String, repoName::String)
+  templateGithub = Template(;
+    user = userName,
+    dir = "~/localgit",
+    julia = v"1",  # for [compat] section in Project.toml
+    plugins = [
+      ProjectFile(; version = v"0.1.0-DEV"),
+      License(; name = "MIT", path = nothing, destination = "LICENSE"),
+      Git(;
+        branch = LibGit2.getconfig("init.defaultBranch", "master"),
+        ssh = true,
+        jl = true,
+        manifest = false),
+      GitHubActions(;
+        destination = "CI.yml",
+        linux = true,
+        osx = false,
+        windows = false,
+        x64 = true,
+        x86 = false,
+        coverage = true,
+        extra_versions = ["1.8", "1.9", "nightly"]),
+      CompatHelper(; destination = "CompatHelper.yml", cron = "0 0 * * *"),
+      TagBot(;
+        destination = "TagBot.yml",
+        trigger = "JuliaTagBot",
+        token = Secret("GITHUB_TOKEN"),
+        ssh = Secret("DOCUMENTER_KEY"),
+        ssh_password = nothing,
+        changelog = nothing,
+        changelog_ignore = nothing,
+        gpg = nothing,
+        gpg_password = nothing,
+        registry = nothing,
+        branches = nothing,
+        dispatch = nothing,
+        dispatch_delay = nothing),
+      Codecov(),
+      Documenter{GitHubActions}(logo = Logo(;
+        light = homedir() * "/.config/logo/logo.png",
+        dark = homedir() * "/.config/logo/logo-dark.png")),
+      Dependabot(),
+    ])
+  generate(templateGithub, repoName)
 end
 
 atreplinit() do repl
@@ -81,7 +64,6 @@ atreplinit() do repl
     @eval using Revise
     @eval using REPLVim
     @eval @async REPLVim.serve()
-    @eval genGithubRepo()
   catch e
     @warn "error while loading OhMyREPL, Revise and REPLVim" e
   end
