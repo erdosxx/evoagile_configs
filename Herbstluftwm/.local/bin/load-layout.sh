a8ec504c1d3f36419d4ea60590324e85d63594ed
Norel <norel.evoagile@gmail.com>
2023-09-14 16:39:45 +0900

    Update settings for rofi and polybar

--- Herbstluftwm/.local/bin/load-layout.sh
+++ Herbstluftwm/.local/bin/load-layout.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-SEL="$(ls ~/.config/herbstluftwm/layouts/ | dmenu -i -fn Hermit:bold -l 5 -p "Which Layout?: " -nb '#000000' -sb '#000000' -sf '#5f875f' -nf '#b8b8b8')"
+DMENU="$HOME/.local/bin/dmenu"
+
+SEL="$(ls ~/.config/herbstluftwm/layouts/ | $DMENU -i -fn Hermit:bold -l 5 -p "Which Layout?: " -nb '#000000' -sb '#000000' -sf '#5f875f' -nf '#b8b8b8')"
 herbstclient load "$(cat ~/.config/herbstluftwm/layouts/$SEL)"
