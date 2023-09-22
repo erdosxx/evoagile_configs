d487e72f667dcf72eed3c15e655672e779dc78e9
Norel <norel.evoagile@gmail.com>
2023-09-22 21:01:23 +0900

    Add reset key mapping with Herbstluftwm key binding.

--- xmodmap/.local/bin/reset_keymap.sh
+++ xmodmap/.local/bin/reset_keymap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
-xmodmap "$HOME/.config/xmodmap/xmodmap_origin.conf"
-sh "$HOME/.config/xmodmap/swap_keys.sh"
+# xmodmap "$HOME/.config/xmodmap/xmodmap_origin.conf"
+setxkbmap -layout gb -option ctrl:swapcaps -option altwin:swap_lalt_lwin -option korean:ralt_hangul -option korean:rctrl_hangul -model latitude
+
+sh "$HOME/.local/bin/swap_keys.sh"
+notify-send "Keymap reset to default"
