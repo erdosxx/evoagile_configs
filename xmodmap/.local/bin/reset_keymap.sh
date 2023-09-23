b2f5ae604f591eea38f603f6a619e4469500df9e
Norel <norel.evoagile@gmail.com>
2023-09-23 15:02:22 +0900

    Update reset keymap for Herbstluft and make silent when use it.

--- xmodmap/.local/bin/reset_keymap.sh
+++ xmodmap/.local/bin/reset_keymap.sh
@@ -4,4 +4,4 @@
 setxkbmap -layout gb -option ctrl:swapcaps -option altwin:swap_lalt_lwin -option korean:ralt_hangul -option korean:rctrl_hangul -model latitude
 
 sh "$HOME/.local/bin/swap_keys.sh"
-notify-send "Keymap reset to default"
+# notify-send "Keymap reset to default"
