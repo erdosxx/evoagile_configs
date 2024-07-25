1c56a72f50add5720d39fd2c2dbbb1c13c5dd93d
Norel <norel.evoagile@gmail.com>
2024-07-25 11:43:47 +0900

    Update wezterm, alacritty and polybar settings.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -43,7 +43,7 @@ end
 -- For example, changing the color scheme:
 config.color_scheme = "nord"
 
-config.font_size = 22.0
+config.font_size = 13.0
 --[[ config.font = wezterm.font("JetBrainsMono Nerd Font Mono") ]]
 --[[ config.font = wezterm.font("FiraCode Nerd Font Mono") ]]
 config.font = wezterm.font("JuliaMono Nerd Font Mono")
@@ -161,6 +161,21 @@ config.keys = {
 		key = "s",
 		mods = os_mod_key,
 		action = act.Search({ CaseInSensitiveString = "" }),
+	},
+	{
+		key = "K",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.IncreaseFontSize,
+	},
+	{
+		key = "J",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.DecreaseFontSize,
+	},
+	{
+		key = ")",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ResetFontAndWindowSize,
 	},
 }
 
